dcb647b0ddafa9f2417982631d2ee01aa8cdae1e
vinhthanh89 <vinhthanh89@gmail.com>
2024-02-04 23:24:41 +0700

    fix

--- src/Components/ProductCard/index.js
+++ src/Components/ProductCard/index.js
@@ -3,20 +3,20 @@ import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
 
 const ProductCard = ({ props }) => {
-  const { image, title, description, price,kind , id } = props;
+  const { image, title, description, price, kind , id } = props;
   const navigative = useNavigate()
 
   return (
     <div className="productcard-container">
       <div className="productcard__image">
-        <div onClick={() => navigative(`${id}`)} className="productcard__image-container">
+        <div onClick={() => navigative(`/order/${kind}/${id}`)} className="productcard__image-container">
           <img src={`${image}`} alt="" />
         </div>
       </div>
       <div className="productcard__info">
         <div className="productcard__info-container">
           <div className="productcard__info--title">
-            <div onClick={() => navigative(`/${id}`)}  className="info--title">
+            <div onClick={() => navigative(`/order/${kind}/${id}`)}  className="info--title">
               <Link to={`hot-deal/${id}`}>
                 <span>{title}</span>
               </Link>
